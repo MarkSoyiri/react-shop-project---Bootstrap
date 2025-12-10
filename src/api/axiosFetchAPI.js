5db1610abf49c19cc98bc31d1c829b57141a6912
MarkSoyiri <marksoyiri001@gmail.com>
2025-12-10 20:12:54 +0000

    up

--- src/api/axiosFetchAPI.js
+++ src/api/axiosFetchAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosFetch = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: process.env.EXPRESS_BACKEND_ONLINE_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
