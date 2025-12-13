c90477cb1a4db216dfe5f08ce6e5ad8d188f7322
MarkSoyiri <marksoyiri001@gmail.com>
2025-12-13 17:05:12 +0000

    up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,7 +34,7 @@ function Login () {
                 return;
             }
     
-            const response = await axiosFetch.post('http://express-js-on-vercel-liart-chi.vercel.app/user/login', {
+            const response = await axiosFetch.post('https://express-js-on-vercel-liart-chi.vercel.app/user/login', {
                 email:Email,
                 password:Password
             })
