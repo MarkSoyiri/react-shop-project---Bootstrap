5748ec822f3d1719bcb35d882ef31465813badb7
Mark Soyiri <marksoyiri001@gmail.com>
2026-08-04 04:50:01 +0000

    feat: make app installable PWA with zesty cave logo favicon

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'icons/zestylogo.png'],
+      manifest: {
+        name: 'Zesty Cave Restaurant',
+        short_name: 'Zesty Cave',
+        description: 'Order from Zesty Cave Restaurant',
+        theme_color: '#d3232a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
+      }
+    })
+  ],
   build: {
     outDir: 'dist'
   }
